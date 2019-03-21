c4c1345c3d4889949a36a65496837603c5d2d7e0
Hernavch <hernandezvc1292@gmail.com>
2019-03-20 22:54:13 -0700

    mysql connected

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,4 +1,38 @@
-var inquirer = require('inquirer');
-
 var mysql = require('mysql');
 
+var inquirer = require('inquirer');
+
+// Establish connection to mysql Database
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password: 'password',
+    database:'bamazon_DB'
+
+});
+
+connection.connect();
+// Run a query to pull up all items currently being sold in database
+connection.query('SELECT * FROM products', function (error, results, fields){
+    if (error) throw error;
+    // Loop through all items and show ID number, item name and price
+    for(var i= 0; i< 10; i++){
+        var itemID = results[i].item_id;
+        var name = results[i].product_name;
+        var price = results[i].price + "$";
+    console.log(itemID, name, price);
+    }
+});
+
+// Use inquirer to prompt user to choose an item for purchase.
+
+inquirer 
+    .prompt([
+        {
+            name: "purchase",
+            type:"input",
+            message:"Which item would you like to purchase? Enter ID#"
+        }
+    ]).then(function(answer){
+        
+    })
